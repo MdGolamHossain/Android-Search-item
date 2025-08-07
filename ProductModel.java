bba861e4cf96839aedeab577571daef9f5dbf987
Md. Golam Hossain <124657954+MdGolamHossain@users.noreply.github.com>
2025-08-07 18:53:21 +0600

    Create ProductModel.java

---
+++ ProductModel.java
@@ -0,0 +1,16 @@
+package packagehere;
+public class ProductModel {
+
+
+        String id, name;
+
+        public ProductModel(String id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        // Getter methods
+        public String getId() { return id; }
+        public String getName() { return name; }
+
+}
